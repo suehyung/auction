1182582e87cf9f8c0bf1b3e041b3ee18e10ba0b7
suehyung <suehyung.shin@gmail.com>
2018-08-06 11:09:35 -0700

    Build basic app structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Account from './Account'
+import BidHistory from './BidHistory'
+import Home from './Home'
+import Login from './Login'
+import Menu from './Menu'
+import Nav from './Nav'
+import Positions from './Positions'
+import Watchlist from './Watchlist'
 import './App.css'
 
 class App extends Component {
   render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className='container'>
+          <Nav />
+          <div className='body-container'>
+            <Menu />
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route path='/positions' component={Positions} />
+              <Route path='/watchlist' component={Watchlist} />
+              <Route path='/bidhistory' component={BidHistory} />
+              <Route path='/account' component={Account} />
+              <Route path='/login' component={Login} />
+              <Route render={function () {
+                return <p>Page Not Found</p>
+              }} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
     )
   }
 }
