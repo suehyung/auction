24b7d9f5c734284542d218974a7f138f8c334df7
suehyung <suehyung.shin@gmail.com>
2018-08-07 14:08:14 -0700

    Create placeholder Players component

---
+++ src/components/Players.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Players extends Component {
+  render () {
+    return (
+      <div className='container'>
+        <div className='positions'>
+          Players
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Players
