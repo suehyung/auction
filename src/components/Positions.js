e977c86bb0773bf8febbf55c9b8dca84ed552876
suehyung <suehyung.shin@gmail.com>
2018-08-07 20:59:52 -0700

    Add function to map over array of player objects

--- src/components/Positions.js
+++ src/components/Positions.js
@@ -1,5 +1,37 @@
 import React, { Component } from 'react'
-import Players from './Players'
+import PlayerBid from './PlayerBid'
+
+// temp player values
+const listOfPlayers = [
+  {name: 'Max Scherzer', closes: '12d 13h 36m', price: '$12.40'},
+  {name: 'Madison Bumgarner', closes: '12d 13h 36m', price: '$9.40'},
+  {name: 'Clayton Kershaw', closes: '12d 13h 36m', price: '$11.80'},
+  {name: 'Shohei Ohtani', closes: '12d 13h 36m', price: '$10.60'},
+  {name: 'James Paxton', closes: '12d 13h 36m', price: '$10.00'},
+  {name: 'Charlie Morton', closes: '12d 13h 36m', price: '$2.40'},
+  {name: 'Noah Syndergaard', closes: '12d 13h 36m', price: '$.80'},
+  {name: 'Jake deGrom', closes: '15d 13h 36m', price: '$.00'},
+  {name: 'Carlos Carrasco', closes: '15d 13h 36m', price: '$10.40'},
+  {name: 'Felix Hernandez', closes: '15d 13h 36m', price: '$12.20'},
+  {name: 'German Marquez', closes: '15d 13h 36m', price: '$5.00'},
+]
+
+function PlayerRow (props) {
+  return (
+    <ul className='list-container'>
+      {props.players.map(function (player, index) {
+        return (
+          <li className='list-row' key={player.name}>
+            <div className='list-name'>{player.name}
+              <div className='heart'></div></div>
+            <div className='list-closes'>{player.closes}</div>
+            <div className='list-price'>{player.price}</div>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
 
 class Positions extends Component {
   render () {
@@ -35,9 +67,15 @@ class Positions extends Component {
               <option value='RP'>RP</option>
             </select>
           </div>
+          <div className='list-title'>
+            <div className='list-title-name'>NAME</div>
+            <div className='list-closes'>CLOSES</div>
+            <div className='list-price'>PRICE</div>
+          </div>
+          <PlayerRow players = {listOfPlayers} />
         </div>
         <div className='rightpane'>
-          <Players />
+          <PlayerBid />
         </div>
       </div>
     )
