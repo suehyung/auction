f504d5188e4a9021c3e04962a7d3acfc19718090
suehyung <suehyung.shin@gmail.com>
2018-08-12 17:06:55 -0700

    Add close window icon and dollar sign in bid form

--- src/components/PlayerBid.js
+++ src/components/PlayerBid.js
@@ -4,7 +4,9 @@ class PlayerBid extends Component {
   render () {
     return (
       <div className='bid-container'>
-        <div>Close Window</div>
+        <svg height='36px' width='36px' className='close-window' version='1.1' viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'>
+          <path d='M17.459,16.014l8.239-8.194c0.395-0.391,0.395-1.024,0-1.414c-0.394-0.391-1.034-0.391-1.428,0  l-8.232,8.187L7.73,6.284c-0.394-0.395-1.034-0.395-1.428,0c-0.394,0.396-0.394,1.037,0,1.432l8.302,8.303l-8.332,8.286  c-0.394,0.391-0.394,1.024,0,1.414c0.394,0.391,1.034,0.391,1.428,0l8.325-8.279l8.275,8.276c0.394,0.395,1.034,0.395,1.428,0  c0.394-0.396,0.394-1.037,0-1.432L17.459,16.014z' fill='gray' />
+        </svg>
         <div className='white box'>
           <div className='player-name'>Madison Bumgarner</div>
           <div className='player-team'>San Francisco Giants</div>
@@ -21,11 +23,14 @@ class PlayerBid extends Component {
         </div>
         <form className='bid-form'>
           <label for='enter-bid'>Enter your bid</label>
-          <input
-            type='number'
-            step='.20' min='.40' max='40' data-number-to-fixed='2' id='enter-bid'
-            name='bid'
-            placeholder='$.20 increments only'/>
+          <div className='dollar'>
+            <i>$</i>
+            <input
+              type='number'
+              step='.20' min='.40' max='40.00'
+              id='enter-bid'
+              placeholder='.40'/>
+          </div>
           <input type='submit' value='PLACE BID' className='bid-button' />
         </form>
       </div>
