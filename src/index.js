4018e0902ccfaa9d8ebd14f249f1c883602e2554
suehyung <suehyung.shin@gmail.com>
2018-08-16 21:00:19 -0700

    Add initial Prisma application server

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
 import registerServiceWorker from './utils/registerServiceWorker'
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-client-preset'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>, 
+  document.getElementById('root')
+)
 registerServiceWorker()
