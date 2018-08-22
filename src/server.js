96debc3a08e79b86b65e67311adee0973df341a3
suehyung <suehyung.shin@gmail.com>
2018-08-21 20:26:01 -0700

    Update resolvers and connect to database

--- src/server.js
+++ src/server.js
@@ -1,70 +1,71 @@
 const { GraphQLServer } = require('graphql-yoga')
-
-// temporarily define in memory since no database set up yet
-let players = [{
-  id: 0,
-  name: 'Madison Bumgarner',
-  team: 'SF',
-  position: 'SP',
-  closingtime: '',
-  price: '',
-  bidder: '',
-  maxbid: '',
-  maxbidder: ''
-}]
-
-let idCount = players.length
+const { Prisma } = require('prisma-binding')
 
 const resolvers = {
   Query: {
     info: () => `This is the player auction API`,
-    feed: () => players,
-    player: (_, {id}) => players.find(player => player.id === id)
+    feed: (root, args, context, info) => {
+      return context.db.query.players({}, info)
+    }
+    // player: (_, {id}) => players.find(player => player.id === id)
   },
 
   Mutation: {
-    post: (root, args) => {
-      const player = {
-        id: `${idCount++}`, //Fix this for unique ID
-        name: args.name,
-        team: args.team,
-        position: args.position,
-        closingtime: args.closingtime,
-        price: args.price,
-        bidder: args.bidder,
-        maxbid: args.maxbid,
-        maxbidder: args.maxbidder
-      }
-      players.push(player)
-      return player
-    },
-
-    updatePlayer: (root, args) => {
-      const player = {
-        id: args.id,
-        name: args.name,
-        team: args.team,
-        position: args.position,
-        closingtime: args.closingtime,
-        price: args.price,
-        bidder: args.bidder,
-        maxbid: args.maxbid,
-        maxbidder: args.maxbidder
-      }
-      players.splice(args.id, 1, player)
-      return player
-    },
-
-    deletePlayer: (root, args) => {
-      players.splice(args.id, 1)
-      return console.log(`Item ${args.id} deleted`)
-      // Problem: if delete element, then index will not match id
-      // Maybe make splice find indexof args.id here and in updatePlayer
+    post: (root, args, context, info) => {
+      return context.db.mutation.createPlayer({
+        data: {
+          name: args.name,
+          team: args.team,
+          position: args.position,
+          closingtime: args.closingtime,
+          price: args.price,
+          bidder: args.bidder,
+          maxbid: args.maxbid,
+          maxbidder: args.maxbidder
+        }
+      }, info)
     }
   }
+
+    // updatePlayer: (root, args) => {
+    //   const player = {
+    //     id: args.id,
+    //     name: args.name,
+    //     team: args.team,
+    //     position: args.position,
+    //     closingtime: args.closingtime,
+    //     price: args.price,
+    //     bidder: args.bidder,
+    //     maxbid: args.maxbid,
+    //     maxbidder: args.maxbidder
+    //   }
+    //   players.splice(args.id, 1, player)
+    //   return player
+    // },
+
+    // deletePlayer: (root, args) => {
+    //   players.splice(args.id, 1)
+    //   return console.log(`Item ${args.id} deleted`)
+    //   // Problem: if delete element, then index will not match id
+    //   // Maybe make splice find indexof args.id here and in updatePlayer
+    // }
+
 }
 
 const typeDefs = './src/schema.graphql'
 const options = { port: 4000 }
-const server = new GraphQLServer({ typeDefs, resolvers })
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql',
+      endpoint: 'https://ibl02-auction.herokuapp.com/auction/dev',
+      secret: 'mysecret123',
+      debug: true
+    })
+  })
+})
+
 server.start(options, () => console.log('Server is running on localhost:' + options.port))
