b7ee6b9070c759f7f10e3cbccd1b01db07a3bbb9
suehyung <suehyung.shin@gmail.com>
2018-08-28 14:24:14 -0700

    Change import to apollo-boost and clean up code

--- src/index.js
+++ src/index.js
@@ -4,19 +4,18 @@ import './index.css'
 import App from './components/App'
 import registerServiceWorker from './utils/registerServiceWorker'
 import { ApolloProvider } from 'react-apollo'
-import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-client-preset'
-
-const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
+import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost'
 
 const client = new ApolloClient({
-  link: httpLink,
+  link: new HttpLink({ uri: 'http://localhost:3000' }),
   cache: new InMemoryCache()
 })
 
-ReactDOM.render(
+const ApolloApp = (
   <ApolloProvider client={client}>
     <App />
-  </ApolloProvider>, 
-  document.getElementById('root')
+  </ApolloProvider>
 )
+
+ReactDOM.render(ApolloApp, document.getElementById('root'))
 registerServiceWorker()
