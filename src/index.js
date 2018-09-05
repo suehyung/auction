f47599a285dd3d946b86a766fe4ea1b44f68d903
suehyung <suehyung.shin@gmail.com>
2018-09-05 16:54:55 -0700

    Minor refactor of index.js

--- src/index.js
+++ src/index.js
@@ -7,15 +7,14 @@ import { ApolloProvider } from 'react-apollo'
 import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost'
 
 const client = new ApolloClient({
-  link: new HttpLink({ uri: 'http://localhost:3000' }),
+  link: new HttpLink({ uri: 'https://ibl02-auction.herokuapp.com/auction/dev' }),
   cache: new InMemoryCache()
 })
 
-const ApolloApp = (
+ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
-  </ApolloProvider>
+  </ApolloProvider>,
+  document.getElementById('root')
 )
-
-ReactDOM.render(ApolloApp, document.getElementById('root'))
 registerServiceWorker()
