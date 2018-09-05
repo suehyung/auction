51f948caf57a0da1718d104aa670d08bd85a99ef
suehyung <suehyung.shin@gmail.com>
2018-09-05 16:59:04 -0700

    Create Watchlist component with basic GraphQL query

--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -1,13 +1,84 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { Query } from 'react-apollo'
+import { gql } from 'apollo-boost'
+import PlayerRow from './PlayerRow'
+import PlayerBid from './PlayerBid'
 
-class Watchlist extends Component {
-  render () {
-    return (
-      <div className='watchlist'>
-        Watchlist here
-      </div>
-    )
+const PLAYERS_LIST = gql`
+  {
+    players {
+      id
+      name
+      team
+      position
+      closingtime
+      price
+    }
   }
-}
+`
+
+const Watchlist = () => (
+  <Query query={PLAYERS_LIST}>
+    {({ error, loading, data }) => {
+      if (error) return `💩 Oops! ${error.message}`
+      if (loading) return 'Patience young grasshopper...'
+
+      return (
+        <div className='main-container'>
+          <div className='leftpane'>
+            <div className='drop-menu'>
+              <select className='dropdown drop-filter'>
+                <option value=''></option>
+                <option value='All'>All</option>
+                <option value='R1'>R1</option>
+                <option value='R2'>R2</option>
+                <option value='R3'>R3</option>
+                <option value='R4'>R4</option>
+              </select>
+              <select className='dropdown grey'>
+                <option value=''>SORT BY</option>
+                <option value='Name'>Name</option>
+                <option value=''>Closing</option>
+                <option value=''>Price</option>
+              </select>
+              <select className='dropdown purple'>
+                <option value=''>POSITION</option>
+                <option value='C'>C</option>
+                <option value='1B'>1B</option>
+                <option value='2B'>2B</option>
+                <option value='SS'>SS</option>
+                <option value='3B'>3B</option>
+                <option value='CF'>CF</option>
+                <option value='OF'>OF</option>
+                <option value='DH'>DH</option>
+                <option value='SP'>SP</option>
+                <option value='RP'>RP</option>
+              </select>
+            </div>
+            <div className='list-title'>
+              <div className='list-title-name'>NAME</div>
+              <div className='list-closes'>CLOSES</div>
+              <div className='list-price'>PRICE</div>
+            </div>
+            <PlayerRow players = {data.players} />
+          </div>
+          <div className='rightpane'>
+            <PlayerBid />
+          </div>
+        </div>
+      )
+    }}
+  </Query>
+)
+
+// class Watchlist extends Component {
+//   render () {
+//     return (
+//       <div className='watchlist'>
+//         Watchlist here
+//       </div>
+//     )
+//   }
+// }
 
 export default Watchlist
