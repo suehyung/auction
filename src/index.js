564466161a3c17ed3cfecbb858d10ea07ba75eac
suehyung <suehyung.shin@gmail.com>
2018-09-25 19:02:00 -0700

    Retrieve token in http header

--- src/index.js
+++ src/index.js
@@ -5,9 +5,25 @@ import App from './components/App'
 import registerServiceWorker from './utils/registerServiceWorker'
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost'
+import { setContext } from 'apollo-link-context'
+import { AUTH_TOKEN } from './constants'
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
+const httpLink = new HttpLink({
+  uri: 'https://ibl02-auction.herokuapp.com/auction/dev'
+})
 
 const client = new ApolloClient({
-  link: new HttpLink({ uri: 'https://ibl02-auction.herokuapp.com/auction/dev' }),
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 })
 
