2bd6d7e6cc717a1513278fd55e3ce296e4596ed8
suehyung <suehyung.shin@gmail.com>
2018-09-25 19:00:42 -0700

    Add login-logout link to title bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
+import { AUTH_TOKEN } from '../constants'
 
 class Nav extends Component {
   render () {
+    const authToken = localStorage.getItem(AUTH_TOKEN)
+
     return (
       <div className='title-container'>
         <div className='title'>
           <div className='title-light'>INTERNET BASEBALL LEAGUE</div>
           <div className='title-dark'>PRESEASON AUCTION</div>
+        </div>
+        <div className='title-login'>
+          {authToken ? (
+            <div
+              onClick={() => {
+                localStorage.removeItem(AUTH_TOKEN)
+                this.props.history.push('/')
+              }}
+            >
+              Logout
+            </div>
+          ) : (
+            <NavLink to='/login'>
+              Login
+            </NavLink>
+          )}
         </div>
       </div>
     )
