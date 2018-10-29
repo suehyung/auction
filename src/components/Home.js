256cceae0198d6020c686d4616981bfc001266f8
suehyung <suehyung.shin@gmail.com>
2018-10-28 19:51:19 -0700

    Adjust styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,22 +5,7 @@ class Home extends Component {
     return (
       <div className='main-container'>
         <div className='leftpane'>
-          <div className='drop-menu'>
-            <select className='dropdown purple'>
-              <option value=''>POSITION</option>
-              <option value='C'>C</option>
-              <option value='1B'>1B</option>
-              <option value='2B'>2B</option>
-              <option value='SS'>SS</option>
-              <option value='3B'>3B</option>
-              <option value='CF'>CF</option>
-              <option value='OF'>OF</option>
-              <option value='DH'>DH</option>
-              <option value='SP'>SP</option>
-              <option value='RP'>RP</option>
-            </select>
-          </div>
-          <div className='text'><h2>Welcome</h2>To start, select a position above. You can sort by name, closing date, or price, and filter by closing round.</div>
+          <div className='text'><h2>Welcome</h2>To start, select a position. You can sort by name, closing date, or price, and filter by closing round.</div>
           <div className='text'><h2>Tips</h2>Winning bids will be highlighted:
             <div className='player-row highlight'></div>
             Bidding on a player will automatically add them to your Watchlist. Tap or click on the heart to add/remove a player from your Watchlist:
