9c6cede1eda318783e311d9e8310b1c87e3279a2
suehyung <suehyung.shin@gmail.com>
2018-11-06 17:55:58 -0800

    Refactor to stateless functional component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Home extends Component {
-  render () {
-    return (
-      <div className='main-container'>
-        <div className='leftpane'>
-          <div className='text'><h2>Welcome</h2>To start, select a position. You can sort by name, closing date, or price, and filter by closing round.</div>
-          <div className='text'><h2>Tips</h2>Winning bids will be highlighted:
-            <div className='player-row highlight'></div>
-            Bidding on a player will automatically add them to your Watchlist. Tap or click on the heart to add/remove a player from your Watchlist:
-            <div className='player-row grey'></div>
-          </div>
-          <div className='text'><h2>Bidding Rules</h2>
-            <ul>
-              <li>Enter your maximum bid and the auction will automatically bid for you up to that maximum, so the price will be the 2nd highest bid plus the bid increment</li>
-              <li>Bid in $.20 increments only; minimum bid is $.40</li>
-              <li>In the event of identical bids, the earlier bid wins</li>
-              <li>Don't wait to bid last minute, as you might be outbid by existing maximum bids, and also miss out on bidding for comparable players</li>
-              <li>All bids are final</li>
-            </ul>
-          </div>
+function Home (props) {
+  return (
+    <div className='main-container'>
+      <div className='leftpane'>
+        <div className='text'><h2>Welcome</h2>To start, select a position. You can sort by name, closing date, or price, and filter by closing round.</div>
+        <div className='text'><h2>Tips</h2>Winning bids will be highlighted:
+          <div className='player-row highlight'></div>
+          Bidding on a player will automatically add them to your Watchlist. Tap or click on the heart to add/remove a player from your Watchlist:
+          <div className='player-row grey'></div>
         </div>
-        <div className='rightpane'> </div>
+        <div className='text'><h2>Bidding Rules</h2>
+          <ul>
+            <li>Enter your maximum bid and the auction will automatically bid for you up to that maximum, so the price will be the 2nd highest bid plus the bid increment</li>
+            <li>Bid in $.20 increments only; minimum bid is $.40</li>
+            <li>In the event of identical bids, the earlier bid wins</li>
+            <li>Don't wait to bid last minute, as you might be outbid by existing maximum bids, and also miss out on bidding for comparable players</li>
+            <li>All bids are final</li>
+          </ul>
+        </div>
       </div>
-
-    )
-  }
+      <div className='rightpane'> </div>
+    </div>
+  )
 }
 
 export default Home
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { getToken, getUser, removeToken } from './User'
 import { Query } from 'react-apollo'
@@ -14,45 +14,41 @@ const USER_TEAM = gql`
   }
 `
 
-class Nav extends Component {
-  render () {
-    const { onAuth } = this.props
-
-    return (
-      <div className='title-container'>
-        <div className='title'>
-          <div className='title-light'>INTERNET BASEBALL LEAGUE</div>
-          <div className='title-dark'>PRESEASON AUCTION</div>
-        </div>
-        <div className='title-login'>
-          {getToken()
-            ? <div onClick={() => {
-              removeToken()
-              this.props.history.push(`/`)
-            }}>
-              <b>Logout</b> (
-              <Query
-                query={USER_TEAM}
-                variables={{userId}}
-              >
-                {({ error, loading, data }) => {
-                  if (error) return (
-                    <span>`Oops! ${error.message}`</span>
-                  )
-                  if (loading) return (
-                    <span>Loading...</span>
-                  )
-                  onAuth.bind(null, data.getuser.team)
-                  return <span>{data.getuser.team}</span>
-                }}
-              </Query>)
-            </div>
-            : <NavLink to='/login'>Login</NavLink>
-          }
-        </div>
+function Nav (props) {
+  return (
+    <div className='title-container'>
+      <div className='title'>
+        <div className='title-light'>INTERNET BASEBALL LEAGUE</div>
+        <div className='title-dark'>PRESEASON AUCTION</div>
       </div>
-    )
-  }
+      <div className='title-login'>
+        {getToken()
+          ? <div onClick={() => {
+            removeToken()
+            props.history.push(`/`)
+          }}>
+            <b>Logout</b> (
+            <Query
+              query={USER_TEAM}
+              variables={{userId}}
+            >
+              {({ error, loading, data }) => {
+                if (error) return (
+                  <span>`Oops! ${error.message}`</span>
+                )
+                if (loading) return (
+                  <span>Loading...</span>
+                )
+                props.onAuth.bind(null, data.getuser.team)
+                return <span>{data.getuser.team}</span>
+              }}
+            </Query>)
+          </div>
+          : <NavLink to='/login'>Login</NavLink>
+        }
+      </div>
+    </div>
+  )
 }
 
 export default Nav
