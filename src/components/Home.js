11b4764448bdeb8c7451af41b96fef4f41ecfa1a
suehyung <suehyung.shin@gmail.com>
2018-11-14 17:46:55 -0800

    Add sample player row to home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,52 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function Home (props) {
-  return (
-    <div className='main-container'>
-      <div className='leftpane'>
-        <div className='text'><h2>Welcome, {props.userteam}</h2>To start, select a position. You can sort by name, closing date, or price, and filter by closing round.</div>
-        <div className='text'><h2>Tips</h2>Winning bids will be highlighted:
-          <div className='player-row highlight'></div>
-          Bidding on a player will automatically add them to your Watchlist. Tap or click on the heart to add/remove a player from your Watchlist:
-          <div className='player-row grey'></div>
+class Home extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { toggle: false }
+    this.updateToggle = this.updateToggle.bind(this)
+  }
+
+  updateToggle () {
+    this.setState(() => { return { toggle: !this.state.toggle } })
+  }
+
+  render () {
+    return (
+      <div className='main-container'>
+        <div className='leftpane'>
+          <div className='text'><h2>Welcome, {this.props.userteam}</h2>To start, select a position. You can sort by name, closing date, or price, and filter by closing round.</div>
+          <div className='text'><h2>Tips</h2>Winning bids will be highlighted:
+            <tr className='list-row highlight'>
+              <td className='list-name'>
+                <p>Sid Fernandez</p>
+                {this.state.toggle === true
+                  ? <div className='heart watchlist'
+                    onClick={this.updateToggle}><div></div></div>
+                  : <div className='no-heart watchlist'
+                    onClick={this.updateToggle}><div></div></div>}
+              </td>
+              <td></td>
+              <td className='list-closes'><div>6d 12h 34m</div></td>
+              <td className='list-price'>$8.80</td>
+            </tr>
+            <br></br>
+            <tr>Tap or click on the heart to add or remove a player from your Watchlist.</tr>
+          </div>
+          <div className='text'><h2>Bidding Rules</h2>
+            <ul>
+              <li>Enter your maximum bid and the auction will automatically bid for you up to that maximum, so the price will be the 2nd highest bid plus the bid increment</li>
+              <li>Bid in $.20 increments only; minimum bid is $.40</li>
+              <li>In the event of identical bids, the earlier bid wins</li>
+              <li>Don't wait to bid last minute, as you might be outbid by existing maximum bids, and also miss out on bidding for comparable players</li>
+              <li>All bids are final</li>
+            </ul>
+          </div>
         </div>
-        <div className='text'><h2>Bidding Rules</h2>
-          <ul>
-            <li>Enter your maximum bid and the auction will automatically bid for you up to that maximum, so the price will be the 2nd highest bid plus the bid increment</li>
-            <li>Bid in $.20 increments only; minimum bid is $.40</li>
-            <li>In the event of identical bids, the earlier bid wins</li>
-            <li>Don't wait to bid last minute, as you might be outbid by existing maximum bids, and also miss out on bidding for comparable players</li>
-            <li>All bids are final</li>
-          </ul>
-        </div>
+        <div className='rightpane'> </div>
       </div>
-      <div className='rightpane'> </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Home
