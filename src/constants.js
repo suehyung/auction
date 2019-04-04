020436141847273c78789a743679752d1dae3d2b
suehyung <suehyung.shin@gmail.com>
2019-04-04 09:51:48 -0700

    Add temporary constants

---
+++ src/constants.js
@@ -0,0 +1,17 @@
+const APP_SECRET = 'GraphQL-is-aw3some'
+// old one is 'GraphQL-is-aw3some'
+
+const AUTH_TOKEN = 'auth-token'
+// old one is 'auth-token'
+
+const PRISMA_SECRET = 'mysecret123'
+// old one is 'mysecret123'
+
+const ENDPOINT = 'https://ibl02-auction.herokuapp.com/auction/dev'
+
+module.exports = {
+  APP_SECRET,
+  AUTH_TOKEN,
+  PRISMA_SECRET,
+  ENDPOINT
+}
