c0cb6c93c933704f9277b0f1c782c88b7cf5b653
suehyung <suehyung.shin@gmail.com>
2019-04-04 08:33:54 -0700

    Fix enviro variables

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,13 +1,13 @@
+const APP_SECRET = process.env.APP_SECRET
+const AUTH_TOKEN = process.env.AUTH_TOKEN
+const PRISMA_SECRET = process.env.PRISMA_SECRET
+const ENDPOINT = process.env.ENDPOINT
+
 module.exports = {
-  APP_SECRET: process.env.APP_SECRET,
-  AUTH_TOKEN: process.env.AUTH_TOKEN,
-  PRISMA_SECRET: process.env.PRISMA_SECRET,
-  ENDPOINT: process.env.ENDPOINT
+  APP_SECRET,
+  AUTH_TOKEN,
+  PRISMA_SECRET,
+  ENDPOINT
 }
 
-//  if (process.env.NODE_ENV === 'production') {
-// we are in production - return the prod set of keys
-//  } else {
-// we are in development - return the dev keys
-//  module.exports = require('./dev')
-//  }
+// check ES6 for FE and BE, maybe split into two. Also verify environment variables
